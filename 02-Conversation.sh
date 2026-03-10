375278500df13a2a244044001cf8795cad64c894
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 20:24:26 +0530

    New File add-10:03:26-20:24

---
+++ 02-Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " Suresh : hello Ramesh,how are you ?"
+echo " Ramesh : I am fine Suresh, how are you ?"
+echo " Suresh : I am also fine, what are you doing ?"   
+echo " Ramesh : I am learning bash scripting, it's very interesting what about you ?"
+echo " Suresh : That's great, keep it up !"
