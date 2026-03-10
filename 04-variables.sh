a114b20a51d83919a7c04b161fe4e9c7b2309dc1
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 21:53:37 +0530

    argment file-added @ 031026:21:53

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1    #no space allowed while assigning value to arrigment variable passed
+PERSON2=$2
+
+echo " ${PERSON1} : hello ${PERSON2},how are you ?"
+echo " ${PERSON2} : I am fine ${PERSON1}, how are you ?"
+echo " ${PERSON1} : I am also fine, what are you doing ?"   
+echo " ${PERSON2} : I am learning bash scripting, it's very interesting what about you ?"
+echo " ${PERSON1} : That's great, keep it up !"
