425bc31001551c6dff3fdf1ca67f519a3d71f12c
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 22:06:57 +0530

    file-changes @ 031026:22:06

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #passing the arrigments dinamaically to the script while executing the script
 echo " please enter your user name ::"
+# what ever name we enter it will be stored in USER_NAME variable will read in line 5;
 read USER_NAME
 
 echo "User name is $USER_NAME"
+
+echo " please enter your password ::"
+
+read -s PASSWORD
+# beacause of -s option password will not be visible while typing
