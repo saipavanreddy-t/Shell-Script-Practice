c1a40407d9e528968bbb8eb8b582e36dc527aacb
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 00:46:28 +0530

    File-added @ 031126:00:46

--- 10-installation.sh
+++ 10-installation.sh
@@ -11,3 +11,10 @@ fi
 #please check the exit status of every command u write in the script,
 ## if any command fails then we should not proceed with next command, we should exit the script
 # with proper message
+
+if [ $? -ne 0 ]; then
+    echo "Nginx installation failed, please check the error message and fix it."
+    exit 1
+else
+    echo "Nginx installed successfully."
+fi
