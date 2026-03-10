c0b50d4605db397a1bb095b065a48fef3b673c24
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 00:13:56 +0530

    File-add @ 031126:23:00

---
+++ 09-Condions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Number=$1
+
+#-gt-> greater than
+#-lt-> less than
+#-eq-> equal to
+#-ne-> not equal to
+#-ge-> greater than or equal to
+#-le-> less than or equal to
+
+if [ $Number -gt 20 ]; then
+    echo " Given Number : $Number is greater than 20"
+fi
