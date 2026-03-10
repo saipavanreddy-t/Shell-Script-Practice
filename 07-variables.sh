7e7d9087594201af4e19b60a5e2704242a93b3de
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 23:28:41 +0530

    file-ADD @ 031026:23:25

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ####special variable
 echo " All Arguments passed to script : $@"
-echo" Total number of arguments passed to script : $#"
+echo " Total number of arguments passed to script : $#"
 echo " Name of the script : $0"
 echo "Present working directory : $PWD"
 echo "User executing the script : $USER"
