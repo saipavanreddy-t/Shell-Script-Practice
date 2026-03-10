8b6985e8a81a5742ae372b9e2f1c16237de93bdd
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 22:02:23 +0530

    file-added @ 031026:22:01

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,3 +7,13 @@ echo " ${PERSON2} : I am fine ${PERSON1}, how are you ?"
 echo " ${PERSON1} : I am also fine, what are you doing ?"   
 echo " ${PERSON2} : I am learning bash scripting, it's very interesting what about you ?"
 echo " ${PERSON1} : That's great, keep it up !"
+
+#output[] sh 04-variables.sh trump bush
+ #trump : hello bush,how are you ?
+ #bush : I am fine trump, how are you ?
+ #trump : I am also fine, what are you doing ?
+ #bush : I am learning bash scripting, it's very interesting what about you ?
+ trump : That's great, keep it up !
+
+
+
---
+++ 05-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#passing the arrigments dinamaically to the script while executing the script
+echo " please enter your user name ::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
