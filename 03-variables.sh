1b775cd0a2716cca57b88578f4e16b809a461536
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-10 21:36:20 +0530

    Variable file added @ 031026:21:23

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Suresh #no space allowed while assigning value to variable
+PERSON2=Ramesh
+
+echo " ${PERSON1} : hello ${PERSON2},how are you ?"
+echo " ${PERSON2} : I am fine ${PERSON1}, how are you ?"
+echo " ${PERSON1} : I am also fine, what are you doing ?"   
+echo " ${PERSON2} : I am learning bash scripting, it's very interesting what about you ?"
+echo " ${PERSON1} : That's great, keep it up !"
