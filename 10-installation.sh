f85095b3c043e7e02d6b17abd46637f2523103c4
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 00:36:59 +0530

    File-added @ 031126:00:36

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,8 @@ UserID=$(id -u)
 
 If [ $UserID -ne 0 ]; then
     echo "Please run this script with root user access"
+    exit 1
 fi
 echo "Installing nginx "
-dnf install -y nginx -y
+dnf install nginx -y
+#please check the exit status of every command u write in the script, if any command fails then we should not proceed with next command, we should exit the script with proper message
