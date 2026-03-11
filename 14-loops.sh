0e1a6a1de4c0dc6f1f3b544531fbafa45259c103
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 22:41:46 +0530

    File-added @ 031226:21:51

---
+++ 14-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_Folder="/var/logs/shell-scripts"
+LOGS_FILE="/var/logs/shell-scripts/$0.log"
+
+if [ $USERID-ne 0 ]; then
+    echo "Please run this script as root access" | tee -a $LOGS_FILE
+    exit 1
+    fi
+    mkdir -p $LOGS_Folder
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo "$2.. Failure" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo "$2 successfully."
+fi
+}
+
+for package in $@ #sudo sh 14-loops.sh nginx nodejs mariadb105 php
+
+do
+    dnf install $package -y &>> $LOGS_FILE
+    validate $? "$package installation"
+done
+
+
