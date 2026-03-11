c5216aaac2c572533a0ea04c0a1667ba0a4c0b5c
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 23:17:25 +0530

    File-added @ 031226:23:15

--- 15-loops.sh
+++ 15-loops.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 LOGS_Folder="/var/logs/shell-scripts"
 LOGS_FILE="/var/logs/shell-scripts/$0.log"
 
-if [ $USERID-ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script as root access" | tee -a $LOGS_FILE
     exit 1
     fi
+
     mkdir -p $LOGS_Folder
+
 validate(){
     if [ $1 -ne 0 ]; then
     echo "$2.. Failure" | tee -a $LOGS_FILE
@@ -21,7 +23,7 @@ fi
 for package in $@ #sudo sh 14-loops.sh nginx nodejs mariadb105 php redis  
 
 do
-dnf installed $package -y &>> $LOGS_FILE
+dnf installed $package &>> $LOGS_FILE
 if [ $? -ne 0 ]; then
     echo "$package not installed , installing now" 
     dnf install $package -y &>> $LOGS_FILE
