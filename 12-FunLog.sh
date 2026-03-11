5888de0d6e76b9d2a616cdf8f91a6cb52abb172c
saipavanreddy <saipavanreddy.turaka@outlook.com>
2026-03-11 22:13:37 +0530

    File-added @ 031226:21:51

--- 12-FunLog.sh
+++ 12-FunLog.sh
@@ -5,13 +5,13 @@ LOGS_Folder="/var/logs/shell-scripts"
 LOGS_FILE="/var/logs/shell-scripts/$0.log"
 
 if [ $USERID-ne 0 ]; then
-    echo "Please run this script as root access"
+    echo "Please run this script as root access" | tee -a $LOGS_FILE
     exit 1
     fi
     mkdir -p $LOGS_Folder
 validate(){
     if [ $1 -ne 0 ]; then
-    echo "$2.. Failure"
+    echo "$2.. Failure" | tee -a $LOGS_FILE
     exit 1
 else
     echo "$2 successfully."
@@ -25,9 +25,6 @@ validate $? "NodeJS installation"
 
 dnf install mariadb105 -y &>> $LOGS_FILE
 validate $? "mariadb105 installation"
-
-dnf install mariadb -y &>> $LOGS_FILE
-validate $? "mariadb installation"
 
 dnf install mariadb-client -y &>> $LOGS_FILE
 validate $? "mariadb-client installation"
